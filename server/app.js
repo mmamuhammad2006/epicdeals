1a45b28c5a51243f7348d68da8dba9d4529ae52e
mmamuhammad2006 <168419071+mmamuhammad2006@users.noreply.github.com>
2024-10-12 16:19:56 +0500

    second commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+
+
+app.use(express.static(path.join(__dirname,'../client/public')))
+app.use(expressLayouts);
+
+app.set('views',path.join(__dirname,'../client/views'))
+app.set('layout','layouts/app')
+app.set('view engine','ejs')
+app.get('/',(req,res)=>{
+    res.render('pages/home')
+})
+
+app.listen(5000) 
