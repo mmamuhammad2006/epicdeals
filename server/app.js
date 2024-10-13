6438f1e1c152cf528aac8b22a376f55b2450d850
mmamuhammad2006 <168419071+mmamuhammad2006@users.noreply.github.com>
2024-10-13 19:30:59 +0500

    install vue, body parser in express

--- server/app.js
+++ server/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const cors = require('cors'); 
+const bodyParser = require('body-parser');
+
 const app = express();
 app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use(express.static(path.join(__dirname,'../client/public')))
 app.use(expressLayouts);
